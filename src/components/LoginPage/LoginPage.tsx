480177ab2f977712677be6b128af88d2ad4c930e
Dima Neor <dima.bulatov.deman@gmail.com>
2022-04-18 00:15:33 +0400

    добавлены шрифты и тело страницы авторизации

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,9 +1,25 @@
 import React, {FC} from 'react';
+import './LoginPage.css'
 
 const LoginPage: FC = () => {
     return (
         <div>
-            <h1>Страница авторизации</h1>
+            <div className='welcomeBlock'>
+                <div className='loginBlock'>
+                    <h1>Добро пожаловать</h1>
+                    <p>Авторизуйтесь, чтобы войти в систему</p>
+                    <input type="text" placeholder="E-mail"/> 
+                    <br/>
+                    <input type="text" placeholder="Пароль"/>
+                    <br/>
+                    <button>Авторизация</button>
+                    <a href="/reset_pass">Забыли пароль?</a>
+                </div>
+                <div className="create"><p>Нет учетной записи ?<a href="url">Зарегистрируйте</a></p></div>
+            </div>
+            <div className='leftImage'>
+                <img src='/src/svg/book-lovers-1.svg'/>
+            </div>
         </div>
     );
 };
